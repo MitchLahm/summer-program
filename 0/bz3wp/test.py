51b5e017334e65014f7dc43b836225f7da3be403
MitchLahm <mil39596@bethel.edu>
2017-07-03 14:43:08 -0400

    Adding integrals file to github

--- 0/bz3wp/test.py
+++
@@ -1,6 +0,0 @@
-# Tests molecule.py by calling static method on molecule.xyz
-
-from molecule import Molecule
-
-mol=Molecule.from_file('../../extra-files/molecule.xyz')
-print(mol)
--- extra-programming-projects/gf-matrix/aewiens/f-matrix/examples/h2o/psi/sp/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import re
-
-f = open("output.dat","r").read()
-
-test = r"\s+Total Energy\s+\=\s+(\-\d+\.\d+)"
-
-print(re.findall(test,f))
